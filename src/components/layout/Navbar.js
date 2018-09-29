ff85ceff9aaacd0620ec5e72dd70afb84a157f58
Udeji Francis <francis.udejiofficial@gmail.com>
2018-09-29 06:55:32 +0100

    login button now dynamic to display either login or logout based on authentication status

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import firebase from "firebase"
+import {config} from "../../config";
 
 const styles = {
   borderRadius: {
@@ -7,75 +9,108 @@ const styles = {
   }
 };
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-danger mb-2">
-    <div className="container">
-      <Link className="navbar-brand" to="/">
-        <h4>Dakata Language Hub</h4>
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/translate">
-              Translate
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/blog">
-              Blog
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/forum">
-              Forum
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">
-              Contact
-            </Link>
-          </li>
-        </ul>
+class Navbar extends Component {
 
-        <div
-          className="btn-group ml-auto"
-          role="group"
-          aria-label="Authentication"
-        >
-          <Link
-            style={styles.borderRadius}
-            to="/auth/login"
-            className="btn btn-primary"
-          >
-            Login
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      email: ""
+    }
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+
+    this.verifyUser()
+
+  }
+
+  verifyUser = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) 
+        this.setState({ loggedIn: true, email: user.email })
+    });
+  } 
+
+  render() {
+    const  { loggedIn, email } = this.state
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-danger mb-2">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <h4>Dakata Language Hub</h4>
           </Link>
-          <Link
-            style={styles.borderRadius}
-            to="/auth/signup"
-            className="btn btn-outline-light ml-2"
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            Register
-          </Link>
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/translate">
+                  Translate
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/blog">
+                  Blog
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/forum">
+                  Forum
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+
+            <div
+              className="btn-group ml-auto"
+              role="group"
+              aria-label="Authentication"
+            >
+              <Link
+                style={styles.borderRadius}
+                to="/auth/logout"
+                className="btn btn-primary"
+              >
+                {loggedIn ? `Logout ${email}` : "Login"}
+              </Link>
+              {!loggedIn 
+                ?
+                <Link
+                  style={styles.borderRadius}
+                  to="/auth/signup"
+                  className="btn btn-outline-light ml-2"
+                >
+                  Register
+                </Link>
+                : 
+                ""
+              }
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </nav>
-);
+      </nav>
+    );
+  }
+  }
 
 export default Navbar;
